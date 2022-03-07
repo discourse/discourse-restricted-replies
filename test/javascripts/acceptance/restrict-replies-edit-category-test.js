7352e0e04eef0c2f45faf6d21bc26ee39a3de918
Jarek Radosz <jradosz@gmail.com>
2022-03-07 14:25:33 +0100

    Fix imports

--- test/javascripts/acceptance/restrict-replies-edit-category-test.js
+++ test/javascripts/acceptance/restrict-replies-edit-category-test.js
@@ -1,13 +1,13 @@
 import selectKit from "discourse/tests/helpers/select-kit-helper";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 import { click, currentURL, visit } from "@ember/test-helpers";
+import { test } from "qunit";
 
 acceptance("Restricted Replies Category Edit", function (needs) {
   needs.user();
 
   test("Restricted Editing the category", async function (assert) {
     await visit("/c/bug/edit/security");
-
     await click(".restrict-replies input[type=checkbox]");
 
     const searchPriorityChooser = selectKit(".restrict-replies .multi-select");
@@ -15,7 +15,7 @@ acceptance("Restricted Replies Category Edit", function (needs) {
     await searchPriorityChooser.selectRowByValue(1);
 
     await click("#save-category");
-    assert.equal(
+    assert.strictEqual(
       currentURL(),
       "/c/bug/edit/security",
       "it stays on the edit screen"
