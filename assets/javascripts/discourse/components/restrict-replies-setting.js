bef381573e02ddeb85dbb9b4336875bf8ece71db
David Taylor <david@taylorhq.com>
2024-11-29 15:42:28 +0000

    DEV: Convert to native class syntax (#75)

--- assets/javascripts/discourse/components/restrict-replies-setting.js
+++ assets/javascripts/discourse/components/restrict-replies-setting.js
@@ -2,9 +2,9 @@ import Component from "@ember/component";
 import { makeArray } from "discourse-common/lib/helpers";
 import discourseComputed from "discourse-common/utils/decorators";
 
-export default Component.extend({
+export default class RestrictRepliesSetting extends Component {
   @discourseComputed
   groups() {
     return makeArray(this.site.groups);
-  },
-});
+  }
+}
